7b9456641f61e49744312bd874236d657e1d2089
miao@zeustellus.com <miao@zeustellus.com>
2025-02-07 14:43:38 +0900

    Input and output

---
+++ C++/input_output/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+    
+    std::string full_name;
+    int age3;
+
+    std::cout<< "Please type your full name and age " <<std::endl;
+
+    std::getline(std::cin,full_name);
+
+    std::cin >> age3;
+
+    std::cout<<"hello "<< full_name << "you are " << age3 << " years old!" << std::endl;
+
+    return 0;
+
+}
